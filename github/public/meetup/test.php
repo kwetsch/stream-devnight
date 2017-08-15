f3bcba2f98c8c9f914137e40e1a39bfe7ca714cb
Niklas Heer <niklas.heer@tradebyte.com>
2017-08-15 11:05:51 +0200

    add test file for redirect with php

---
+++ github/public/meetup/test.php
@@ -0,0 +1,13 @@
+<?php
+
+function Redirect($url, $permanent = false)
+{
+    if (headers_sent() === false)
+    {
+        header('Location: ' . $url, true, ($permanent === true) ? 301 : 302);
+    }
+
+    exit();
+}
+
+Redirect('https://www.meetup.com/de-DE/dev_night/events/242557041/', false);
