25745c3e5caefb351bc1b79865a73748249dc1d8
Niklas Heer <me@nheer.io>
2018-01-31 15:50:21 +0100

    Update for no. 19

--- github/public/meetup/index.php
+++ github/public/meetup/index.php
@@ -10,4 +10,4 @@ function Redirect($url, $permanent = false)
     exit();
 }
 
-Redirect('https://www.meetup.com/de-DE/dev_night/events/246289992/', false);
+Redirect('https://www.meetup.com/de-DE/dev_night/events/247317536/', false);
